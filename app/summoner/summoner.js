4f9fd5b269769ca0301b7ade3bf29134250504bf
Salvador Holguin <salvadorhol@gmail.com>
2015-08-04 19:35:18 -0400

    Small Last Min Tweeking
    added a leesin png (borrowed from leagueoflegends.com), modified some
    angular code  will explain during stream

--- app/summoner/summoner.js
+++ app/summoner/summoner.js
@@ -3,12 +3,12 @@
 angular.module('myApp.summoner', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/summoner', {
+  $routeProvider.when('/summoner/:region/:name', {
     templateUrl: 'summoner/summoner.html',
     controller: 'SummonerCtrl'
   });
 }])
 
-.controller('SummonerCtrl', [function() {
-
+.controller('SummonerCtrl', ['$scope', '$routeParams', '$log', function($scope, $routeParams, $log) {
+	$scope.url = $routeParams.region + "/" + $routeParams.name;
 }]);
