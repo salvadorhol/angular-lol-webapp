3777f22911984c535a3220c1cd7a0a91839af950
Salvador Holguin <salvadorhol@gmail.com>
2015-08-11 22:19:33 -0400

    Profile Page
    Showing more details in summoner.html page. Added loading html angular
    effect.

--- app/summoner/summoner.js
+++ app/summoner/summoner.js
@@ -9,9 +9,18 @@ angular.module('myApp.summoner', ['ngRoute'])
   });
 }])
 
-.controller('SummonerCtrl', ['$scope', '$routeParams', '$log', '$http', 'SummonerService', function($scope, $routeParams, $log, $http, SummonerService){
+.controller('SummonerCtrl', ['$scope', '$routeParams', '$log', '$http', '$interval', 'SummonerService', function($scope, $routeParams, $log, $http, $interval, SummonerService){
 	$scope.url = $routeParams.region + "/" + $routeParams.name;
 	console.log($routeParams);
+
+	$scope.loadingDots = "";
+	//for interval, do something every 800 miliseconds
+	var loading = $interval(function(){
+		if($scope.loadingDots.length == 0) $scope.loadingDots = ".";
+		else if($scope.loadingDots.length == 1) $scope.loadingDots = "..";
+		else if ($scope.loadingDots.length == 2) $scope.loadingDots = "...";
+		else $scope.loadingDots = "";
+	}, 250);
 
 	var ajaxData = {region: $routeParams.region, name: $routeParams.name, summoner: SummonerService.summoner};
 	console.log("SummonerCtrl.ajaxData: - "); console.log(ajaxData);
@@ -21,20 +30,24 @@ angular.module('myApp.summoner', ['ngRoute'])
 		.then(function(response){
 				console.log("SummonerCtrl.RiotAPI.getProfile: Response - "); 
 				console.log(response.data);
-				//pre filtering
-				angular.forEach(response.data.league[response.data.id], function(league){
-					//console.log(league);
-					response.data.league[league.queue] = league;
+				
+				//pre filtering for leagues only if not null
+				if(response.data.league){
+					angular.forEach(response.data.league[response.data.id], function(league){
+						//console.log(league);
+						response.data.league[league.queue] = league;
 
-					//if he played and placed into solo queue
-					if(league.queue === "RANKED_SOLO_5x5"){
-						var entries = response.data.league[league.queue].entries;
-						var me = entries[findWithAttr(entries,'playerOrTeamId', response.data.id)];
-						me.rankicon = "img/medals/" + response.data.league[league.queue].tier + me.division + ".png";
-						response.data.league.soloqueue = me;
-					}
-				});
+						//if he played and placed into solo queue
+						if(league.queue === "RANKED_SOLO_5x5"){
+							var entries = response.data.league[league.queue].entries;
+							var me = entries[findWithAttr(entries,'playerOrTeamId', response.data.id)];
+							me.rankicon = "img/medals/" + response.data.league[league.queue].tier + me.division + ".png";
+							response.data.league.soloqueue = me;
+						}
+					});
+				}
 
+				$interval.cancel(loading);
 				$scope.summoner = response.data;
 
 		//when we get something other than 400
